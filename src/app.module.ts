4b10fddccd13360a4a9d7f6c151b6b7ac3f7e21c
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-04-15 14:28:53 -0300

    feat(app): integration for app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InvoicesModule } from './invoices/invoices.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().uri().required(),
+        SUSPICIOUS_VARIATION_PERCENTAGE: Joi.number(),
+        INVOICES_HISTORY_COUNT: Joi.number(),
+        SUSPICIOUS_INVOICES_COUNT: Joi.number(),
+        SUSPICIOUS_TIMEFRAME_HOURS: Joi.number(),
+      }),
+    }),
+    EventEmitterModule.forRoot(),
+    InvoicesModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
